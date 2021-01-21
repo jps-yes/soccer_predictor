8cffbaea628354e0fd3bb4e0cae9096fe897b113
jps-yes <joao.ps.silva@outlook.pt>
2021-01-21 22:57:54 +0000

    update

---
+++ website/app.js
@@ -0,0 +1,23 @@
+getTodayMatches();
+
+async function getTodayMatches() { // add async ????
+	const matches = await fetch("data.csv");
+	//const data = await matches.text();
+	console.log("Hello World");
+}
+
+function readMore() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var readMore = document.getElementById("readMore");
+
+	if (dots.style.display === "none") {
+		dots.style.display = "inline";
+		readMore.innerHTML = "Read more"; 
+		moreText.style.display = "none";
+	} else {
+		dots.style.display = "none";
+		readMore.innerHTML = "Read less"; 
+		moreText.style.display = "inline";
+	}
+}
